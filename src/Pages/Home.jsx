ac5fc09bee0cbc2e633cf86e01d09c37ffb5d55a
Vaishnavi Kotturi <154414648+K-Vaishnavi10@users.noreply.github.com>
2025-03-25 10:27:41 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router";
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Hi!! This is Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
